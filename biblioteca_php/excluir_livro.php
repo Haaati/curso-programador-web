d5cfc45ee0943fc662c973319c9d6ae5db244fb5
Beatriz <beatriz.kava@gmail.com>
2026-06-24 16:53:38 -0300

    update/full_CRUD

---
+++ biblioteca_php/excluir_livro.php
@@ -0,0 +1,10 @@
+<?php 
+
+include("verificar_sessao.php");
+include("conexao.php");
+
+$id = $_GET['id'];
+$sql = "DELETE FROM livros WHERE id=$id";
+mysqli_query($conexao, $sql);
+
+header("Location: listar_livros.php");
